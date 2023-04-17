68241201a5739764fb95165c0496d826df7f14e8
M-Vinayaka <vinayakam@iisc.ac.in>
2023-04-17 23:54:07 +0530

    Documented Newton nD

--- ClassicalMechanicsLean/Newton_nD.lean
+++ ClassicalMechanicsLean/Newton_nD.lean
@@ -12,111 +12,81 @@ namespace Newton_nD
 local infixl:arg (priority := high) "^" => Vector
 
 /--
-A particle is defined to be a structre with 
+A particle is defined to be a structre with mass, position, velocity and a force acting on it.
 -/
 structure Particle (n : ℕ) where
   m : ℝ 
   x : SmoothFunction 1 n 
   v : SmoothFunction 1 n
   F : ℝ^n → ℝ^n 
-  --{h : Vector.cons (v.asFunc) Vector.nil = Vector.get x.grad ⟨0, Nat.zero_lt_succ 0⟩}
-
-/-! We then define particle's velocity and acccelaration as given below
--/
 
 /--
 The acceleration of the particle is defined to be the gradient of the velocity with respect to time.
-It is represented 
+It is a vector, represented by a column matrix. 
 -/
 def Particle.a {n : ℕ} (z : Particle n) : (ℝ → Matrix' n 1 ℝ) := 
   fun (t : ℝ) => (z.v.grad ⟨[t], rfl⟩)
 
+/--
+The momentum of a particle is a SmoothFunction.
+It is defined to be the mass times velocity of the given particle.
+-/
 def Particle.p {n : ℕ} (z : Particle n) : (SmoothFunction 1 n) := 
   ⟨fun (t : ℝ^1) => ((z.v.asFunc t).map ((z.m)*·)),
    fun (t : ℝ^1) => ((z.a (t[0])).map ((z.m)*·)),
    sorry
   ⟩
 
+/--
+System of n particles is defined to be a vector
+-/
 structure System (n : ℕ) (m : ℕ) := 
   particles : Vector (Particle n) m
 
-/-! Sum of mass of particles in a system
+/-- Sum of mass of particles in a system
 -/
 
 def System.m {n : ℕ} {m : ℕ} (S : System n m) : ℝ :=
   S.particles.map Particle.m |>.toList.sum
 
-/--Fext of a system
+/--The total external force acting on a system
 -/
 
 def System.F {n : ℕ} {m : ℕ} (S : System n m) : (ℝ^n → ℝ^n) :=
   fun (x : ℝ^n) => S.particles.map (fun  z => z.F x) |>.toList.sum
 
-/- Initial attempt to define System.m
-
-  match S.particles.toList with
-  | [] => 0
-  | head :: tail =>
-    have h₁ : Nat.succ (List.length tail) = n := by
-      trans 
-      · apply List.length_cons
-      · simp
-    have h₂ : n-1 = List.length tail :=
-      Nat.pred_eq_of_eq_succ h₁.symm
-    head.m + (mSys ⟨⟨tail, h₂.symm⟩, S.Fext⟩)
--/
-
-
-/-! Sum of momentum of particles in a system
+/-- Sum of momentum of particles in a system
 -/
 
 def System.p {n : ℕ} {m : ℕ} (S : System n m) : (SmoothFunction 1 n) :=
   ⟨fun t => S.particles.map (fun z => (z.v.asFunc t).map ((z.m)*·)) |>.toList.sum,
    fun t => S.particles.map (fun z => ((z.a (t[0])).map ((z.m)*·)))|>.toList.sum, sorry⟩
 
-/-! Velocity and accelaration of centre of mass of a system is defined below
+/--
+Velocity of the Centre of Mass of a system
+Declaration uses sorry due to lack of proof of existence of the gradient.
 -/
-
 noncomputable def System.vcom {n : ℕ} {m : ℕ} (S : System n m) : (SmoothFunction 1 n) := 
   ⟨fun t => S.particles.map (fun z => (1/S.m)•(z.v.asFunc t)) |>.toList.sum, 
    fun t => S.particles.map (fun z => (1/S.m)•(z.v.grad t)) |>.toList.sum,
    sorry⟩ 
 
-/-! Initial attempt at defining velocity of centre of mass
-
-  match S.particles.toList with
-  | [] => 0
-  | head :: tail =>
-    have h₁ : Nat.succ (List.length tail) = n := by
-      trans 
-      · apply List.length_cons
-      · simp
-    have h₂ : n-1 = List.length tail :=
-      Nat.pred_eq_of_eq_succ h₁.symm
-    ⟨ fun (t : ℝ^1) =>
-        (((head.m)*(head.v.asFunc t) + (vcom ⟨⟨tail, h₂.symm⟩, S.Fext⟩).asFunc t)/mSys S)
-    , fun (t : ℝ^1) =>
-        let first : ℝ := (head.m)*((head.v.grad t) : ℝ)
-        let second : ℝ := ((vcom ⟨⟨tail, h₂.symm⟩, S.Fext⟩).grad t)
-        show ℝ from first + second⟩
-  | [] => ⟨0, 0⟩
+/--
+Acceleration of the Centre of Mass of a system.
 -/
-
 noncomputable def System.acom {n : ℕ} {m : ℕ} (S : System n m) : (ℝ → Matrix' n 1 ℝ) :=
   fun t => S.particles.map (fun z => (1/S.m)•(z.v.grad t)) |>.toList.sum
 
-/-! We give two of the most fundmental laws of Newton mechanics
--/  
-
+/--A Newtonian System follows Newton's second law and has conservation of momentum-/
 structure NewtonianSystem (n : ℕ) (m : ℕ) extends System n m where 
   Second_Law : (fun (x : ℝ^n) => fun (t : ℝ) => Matrix'.col (toSystem.F x)) = (fun x: ℝ^n => (fun t : ℝ => ((toSystem.m)•(toSystem.acom t))))
   Conservation_of_Momentum :=  (toSystem.F = 0) → (toSystem.p.grad = 0)
 
 
-/-! We define the kinetic energy of a particle
--/
-
+/--The value 1/2 as a real number-/
 def half : ℝ := (1/2 : ℚ)
+
+/--The Kinetic Energy of a particle.-/
 def Particle.Ek {n : ℕ} (z : Particle n) : (ℝ → ℝ) :=
   fun (t : ℝ) => (Vector.map₂ (· * ·) (z.v.asFunc ⟨[t], rfl⟩) (z.v.asFunc ⟨[t], rfl⟩)).toList.sum * (z.m) * (half)
 
