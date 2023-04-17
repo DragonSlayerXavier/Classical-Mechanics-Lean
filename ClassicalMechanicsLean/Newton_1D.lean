ea2cf258bfce4cd8d52149508850dffea413c316
M-Vinayaka <vinayakam@iisc.ac.in>
2023-04-18 00:05:39 +0530

    Documented Newton_1D. Amrit's laptop died

--- ClassicalMechanicsLean/Newton_1D.lean
+++ ClassicalMechanicsLean/Newton_1D.lean
@@ -8,113 +8,74 @@ namespace Newton1
 /-# Newton mechanics in ℝ 
 -/
 
-/-! Should include hypothesis in the definition of Particle without error.
--/
 
+/--
+A particle is defined as a structure with mass, position, velocity and an external force acting on it.
+-/
 structure Particle where
   m : ℝ 
   x : Jet.SmoothFunction 1 
   v : Jet.SmoothFunction 1
   F : ℝ^1 → ℝ
-  --{h : Vector.cons (v.asFunc) Vector.nil = Vector.get x.grad ⟨0, Nat.zero_lt_succ 0⟩}
 
-/-! We then define particle's velocity and acccelaration as given below
+/--
+Acceleration of a particle is defined as the gradient of its velocity at a given time.
 -/
-
 def Particle.a (z : Particle) : (ℝ → ℝ) := 
   fun (t : ℝ) => z.v.grad ⟨[t], rfl⟩ 
 
+/--Momentum of a particle is the product of its mass and velocity-/
 def Particle.p (z : Particle) : (Jet.SmoothFunction 1) := 
   ⟨fun (t : ℝ^1) => ((z.m)*(z.v.asFunc t)),
    fun (t : ℝ^1) =>
      let result := (Jet.Vector.dot (Vector.cons (z.m) Vector.nil) (z.v.grad t))
      ⟨[result], rfl⟩⟩
 
+/--A system is a vector of particles.-/
 structure System (n : ℕ) := 
   particles : Vector Particle n
 
-/-! Sum of mass of particles in a system
+/-- Total mass of particles in a system
 -/
-
 def System.m {n : ℕ} (S : System n) : ℝ :=
   S.particles.map Particle.m |>.toList.sum
 
+/--
+The total external force acting on the system is the sum of the forces acting on the particles.
+-/
 def System.Fext {n : ℕ} (S : System n) : ℝ^1 → ℝ :=
   S.particles.map Particle.F |>.toList.sum
 
-/- Initial attempt to define System.m
-
-  match S.particles.toList with
-  | [] => 0
-  | head :: tail =>
-    have h₁ : Nat.succ (List.length tail) = n := by
-      trans 
-      · apply List.length_cons
-      · simp
-    have h₂ : n-1 = List.length tail :=
-      Nat.pred_eq_of_eq_succ h₁.symm
-    head.m + (mSys ⟨⟨tail, h₂.symm⟩, S.Fext⟩)
+/-- Sum of momentum of particles in a system
 -/
-
-
-/-! Sum of momentum of particles in a system
--/
-
 def System.p {n : ℕ} (S : System n) : (Jet.SmoothFunction 1) :=
   ⟨fun t => S.particles.map (fun p => p.m * p.v.asFunc t) |>.toList.sum,
    fun t => S.particles.map (fun p => ⟨[p.m * p.a t], rfl⟩) |>.toList.sum⟩
 
-/-! Velocity and accelaration of centre of mass of a system is defined below
--/
-
+/--The velocity of the system's centre of mass-/
 noncomputable def System.vcom {n : ℕ} (S : System n) : (Jet.SmoothFunction 1) := 
   ⟨fun t => S.p.asFunc t / S.m, fun t => Vector.cons (S.p.grad t / S.m) Vector.nil⟩ 
 
-/-! Initial attempt at defining velocity of centre of mass
-
-  match S.particles.toList with
-  | [] => 0
-  | head :: tail =>
-    have h₁ : Nat.succ (List.length tail) = n := by
-      trans 
-      · apply List.length_cons
-      · simp
-    have h₂ : n-1 = List.length tail :=
-      Nat.pred_eq_of_eq_succ h₁.symm
-    ⟨ fun (t : ℝ^1) =>
-        (((head.m)*(head.v.asFunc t) + (vcom ⟨⟨tail, h₂.symm⟩, S.Fext⟩).asFunc t)/mSys S)
-    , fun (t : ℝ^1) =>
-        let first : ℝ := (head.m)*((head.v.grad t) : ℝ)
-        let second : ℝ := ((vcom ⟨⟨tail, h₂.symm⟩, S.Fext⟩).grad t)
-        show ℝ from first + second⟩
-  | [] => ⟨0, 0⟩
--/
-
+/--The acceleration of the system's centre of mass is the gradient of the velocity of the same.-/
 noncomputable def System.acom {n : ℕ} (S : System n) : (ℝ → ℝ) :=
   fun (t : ℝ) => (S.vcom.grad ⟨[t], rfl⟩) 
 
-/-- We give two of the most fundmental laws of Newton mechanics
--/  
-
+/--
+A Newtonian System is one with the second law and conservation of momentum.
+-/
 structure NewtonianSystem  (n : ℕ) extends System n where
   Second_Law : fun (x : ℝ^1) => toSystem.particles.map (fun p => p.F x) |>.toList.sum = fun (x : ℝ^1) => m * toSystem.acom x
   Conservation_of_Momentum : (toSystem.Fext = 0) → (toSystem.p.grad = 0)
 
 
-/-- Third Law of Newton mechanics
--/
-
-def hi := 0
-
-/-- We define the kinetic energy of a particle
--/
-
+/--The value 1/2 as a real-/
 def half : ℝ := (1/2 : ℚ)
 
+/--The kinetic energy of a particle.-/
 def Particle.Ek (z : Particle) : (ℝ → ℝ) :=
   fun (t : ℝ) => (z.m)*(((z.v.asFunc ⟨[t], rfl⟩) : ℝ)^2)*(half)
 
-/-! We define the potential energy of a particle
+/-- The potential energy of a particle
 -/
 
 def Particle.Ep (a b : ℝ) (z : Particle) [int : Integrable (z.F : (ℝ → ℝ))] : ℝ := 
