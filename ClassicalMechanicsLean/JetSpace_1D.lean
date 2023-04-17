cd24e8d4517f7b4373bca6c483dd9fce69201cb0
M-Vinayaka <vinayakam@iisc.ac.in>
2023-04-18 00:13:53 +0530

    Documented JetSpace_1D -Amrit, mourning his dead laptop

--- ClassicalMechanicsLean/JetSpace_1D.lean
+++ ClassicalMechanicsLean/JetSpace_1D.lean
@@ -26,18 +26,23 @@ instance  {n : ℕ } : Add  (ℝ ^ n) := ⟨fun v₁ v₂ =>
 
 #check Vector.map₂
 
+/--Addition of Jets is addition of their values and gradients-/
 instance addJets {n: ℕ} : Add (Jet n) := 
     ⟨fun j₁ j₂ => ⟨j₁.value + j₂.value, j₁.gradient + j₂.gradient⟩⟩
 
+/--Scalar Multiplication of ℝ^n-/
 instance scMul {n : ℕ } : SMul ℝ  (ℝ ^ n) := 
   ⟨fun c v => v.map (c * ·)⟩
 
+/--Scalar Multiplication of Jets-/
 instance scMulJets {n : ℕ } : SMul ℝ (Jet n) :=
   ⟨fun c j => ⟨c * j.value, c • j.gradient⟩⟩
 
+/--Dot product of two vectors-/
 def Vector.dot {n: ℕ}(v₁ v₂ : ℝ ^ n) : ℝ := 
   (Vector.map₂ (· * ·) v₁ v₂).toList.sum
 
+/--Product rule of derivatives-/
 instance liebnitz {n: ℕ} : Mul (Jet n) :=
   ⟨fun j₁ j₂ => ⟨j₁.value * j₂.value, j₁.value • j₂.gradient + j₂.value • j₁.gradient⟩⟩
 
@@ -45,19 +50,24 @@ instance liebnitz {n: ℕ} : Mul (Jet n) :=
 /-- Should be replaced by an actual definition eventually -/
 opaque hasGradAt {n: ℕ} (f : ℝ ^ n → ℝ)(x : ℝ ^n) : Prop 
 
-/-- A function `ℝ^n → ℝ` with its gradient, the commented out condition should be added-/
+/-- A function `ℝ^n → ℝ` with its gradient-/
 structure SmoothFunction (n : ℕ) where
   asFunc : ℝ ^ n → ℝ
   grad : ℝ ^ n  → ℝ ^ n 
-  --hasGradAt : ∀ x, hasGradAt jetMap x
 
 instance : CoeFun (SmoothFunction n) (fun _ => ℝ^n → ℝ) where
   coe := SmoothFunction.asFunc
 
-/-- Should be proved as a theorem -/
+/-- 
+The value of gradient is determined by the function.
+Hence if the functional value is same at all points, so are the gradients.
+Hence, we only need to prove equality of the functions to show equality.
+This is axiomatized.
+ -/
 axiom gradient_determined {n: ℕ} (f g : SmoothFunction n) : 
     f.asFunc = g.asFunc → f = g
 
+/--The zero vector, defined inductively-/
 def zeroVector {n : ℕ} : ℝ ^ n := match n with 
   | 0 => Vector.nil
   | n + 1 => Vector.cons 0 (zeroVector : ℝ ^ n)
@@ -68,14 +78,11 @@ def consVector {n : ℕ} (c : ℝ) : ℝ ^ n := match n with
   | 0 => Vector.nil
   | n + 1 => Vector.cons c (zeroVector : ℝ ^ n)
 
-/-
-def Jet.const (n : ℕ) (c : ℝ) : Jet n := 
-  ⟨c, zeroVector⟩
--/
-
+/--A constant SmoothFunction has a constant value and zero gradient-/
 def SmoothFunction.const (n : ℕ) (c : ℝ) : SmoothFunction n := 
   ⟨fun _ => c, fun _ => 0⟩
 
+/---/
 def Vector.coord (i n : ℕ) : (i < n) →  ℝ ^ n :=
   fun h => 
   match i, n, h with 
@@ -90,13 +97,6 @@ def Vector.coord (i n : ℕ) : (i < n) →  ℝ ^ n :=
 def SmoothFunction.coord (i n : ℕ) (h : i < n) : SmoothFunction n := 
   ⟨fun v : Vector ℝ n => v.get ⟨i, h⟩, fun _ => 0⟩
 
-
--- instance : Coe  ℝ  (ℝ ^ 1) := ⟨fun c => Vector.cons c Vector.nil⟩
-/- instance (l : List ℝ) (n : outParam ℕ)
-         (h : outParam (l.length = n) := by rfl)
-    : CoeDep (List ℝ) l (ℝ^n) where
-  coe := ⟨l, h⟩ -/
-
 instance : Coe  (ℝ ^ 1) ℝ  := ⟨fun v => v.get ⟨0, Nat.zero_lt_succ 0⟩⟩
 
 /-- Composition with a smooth function `ℝ → ℝ` with chain rule for derivative -/
@@ -110,6 +110,7 @@ def SmoothFunction.comp {n: ℕ} (g : SmoothFunction 1) (f : SmoothFunction n)  
 
 infix:65 " ∘ " => SmoothFunction.comp
 
+/--Addition of the components of a vector-/
 def addVec {n : ℕ} (v1 : ℝ^n) : ℝ := 
   v1.toList.sum
 
